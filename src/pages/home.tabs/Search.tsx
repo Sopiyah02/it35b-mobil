aac43e8c4510d18f471f2eb8abb6b47580285e62
Sopiyah02 <sopialorrainesolana@gmail.com>
2026-04-21 08:52:59 +0800

    added search content (not finish)
    Added a card component to the Search page with a title, subtitle, and content.

--- src/pages/home.tabs/Search.tsx
+++ src/pages/home.tabs/Search.tsx
@@ -1,14 +1,43 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButtons, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { useEffect, useState } from 'react';
 
 const Search: React.FC = () => {
+    const games = [
+        {name: 'Pokemon Yellow'},
+        {name: 'Mega Man X'},
+        {name: 'The Legend of Zelda'},
+        {name: 'Pac-Man'},
+        {name: 'Super Mario World'}
+    ]
+    const [searchText,setSearchText] = useState("");
+    const [filtered,setFilteredGames] =useState(games);
+    useEffect(()=>{
+        const debounce = setTimeout (()=>{
+            const filtered = games.filter(game=>
+                game.name.toLowerCase().includes(searchText.toLowerCase())
+            );
+            setFilteredGames(filtered);
+        },400);
+        return ()=> clearTimeout(debounce);
+    },[searchText]);
+    
     return (
         <IonPage>
                 <IonHeader>
                     <IonToolbar>
-                        <IonButtons>
+                        <IonButtons slot="start">
                             <IonMenuButton></IonMenuButton>
                         </IonButtons>
                         <IonTitle>Search</IonTitle>
+                         <IonCard>
+      <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/card-media.png" />
+      <IonCardHeader>
+        <IonCardTitle>Card Title</IonCardTitle>
+        <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+      </IonCardHeader>
+
+      <IonCardContent>Here's a small text description for the card content. Nothing more, nothing less.</IonCardContent>
+    </IonCard>
                     </IonToolbar>
                 </IonHeader>
               <IonContent fullscreen>
