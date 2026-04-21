e9596f6ac9d81bac55e3154d849acc4a10997ca0
Sopiyah02 <sopialorrainesolana@gmail.com>
2026-04-21 08:30:15 +0800

    added feed content
    Added a list of games to the Feed component and updated IonContent styling.

--- src/pages/home.tabs/Feed.tsx
+++ src/pages/home.tabs/Feed.tsx
@@ -1,18 +1,48 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButtons, IonContent, IonHeader, IonItem, IonLabel, IonList, IonListHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 
 const Feed: React.FC = () => {
+
+    const games = [
+        {name: 'Pokémon Yellow'},
+        {name: 'Mega Man X'},
+        {name: 'The Legend of Zelda'},
+        {name: 'Pac-Man'},
+         {name: 'Super Mario World'}
+    ]
     return (
         <IonPage>
                 <IonHeader>
                     <IonToolbar>
-                        <IonButtons>
+                        <IonButtons slot="start">
                             <IonMenuButton></IonMenuButton>
                         </IonButtons>
                 <IonTitle>Feed</IonTitle>
                     </IonToolbar>
                 </IonHeader>
-              <IonContent fullscreen>
+              <IonContent className="ion-padding">
+      <IonList>
+      <IonItem>
+        <IonLabel>Pokémon Yellow</IonLabel>
+      </IonItem>
+      <IonItem>
+        <IonLabel>Mega Man X</IonLabel>
+      </IonItem>
+      <IonItem>
+        <IonLabel>The Legend of Zelda</IonLabel>
+      </IonItem>
+      <IonItem>
+        <IonLabel>Pac-Man</IonLabel>
+      </IonItem>
+      <IonItem>
+        <IonLabel>Super Mario World</IonLabel>
+      </IonItem>
+    </IonList>
 
+    {games.map((item, index) => (
+    <IonItem key={index}>
+        <IonLabel>{item.name}</IonLabel>
+    </IonItem>
+    ))}
               </IonContent>
         </IonPage>
 
